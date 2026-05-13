26d499d65daadce31b85096ead8d33022b074024
Ryhox <emanuelpfeifer1@gmail.com>
2026-05-13 19:36:21 +0200

    fix

--- lib/push.ts
+++ lib/push.ts
@@ -38,7 +38,7 @@ export async function requestPermissionAndSubscribe(): Promise<void> {
     if (!sub) {
       sub = await reg.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+        applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as Uint8Array as BufferSource,
       });
     }
 
